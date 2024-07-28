799531d7d8e21dc444227586eca97bcbd33a1816
Pasquale D'Agostino <2095574+elauqsap@users.noreply.github.com>
2024-07-27 19:52:13 -0700

    init configs for lua, starship, tmux, and zsh

---
+++ nvim/lua/plugins/onedark.lua
@@ -0,0 +1,12 @@
+return {
+  -- https://github.com/navarasu/onedark.nvim
+  { "navarasu/onedark.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+      style = "dark",
+    },
+  },
+}
+
